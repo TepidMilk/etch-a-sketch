39c954d49a0cfb4bf539c00580f98fdb50b71bbc
Noah Kingrey <ivorykingrey@gmail.com>
2023-05-02 21:01:05 -0500

    fix createBoard function and add ui for sizing

--- script.js
+++ script.js
@@ -1,11 +1,18 @@
-const pixel = document.createElement('div')
-const boardSpace = document.querySelector('#board')
 
-pixel.classList.add('pixel')
-function createBoard(){
-    for(i = 0; i < 16; i++){
-        boardSpace.appendChild(pixel)
+const boardSpace = document.querySelector('#board')
+const size = document.querySelector('#size')
+
+size.addEventListener('change', function(){
+    while (boardSpace.firstChild) {
+        boardSpace.removeChild(boardSpace.firstChild);
+      }
+    createBoard(size.value)
+})
+
+function createBoard(size){
+    for(i = 0; i <= size; i++){
+        let pixel = document.createElement('div');
+        pixel.textContent = 'Div' + i;
+        boardSpace.appendChild(pixel);
     }
 }
-
-createBoard()
