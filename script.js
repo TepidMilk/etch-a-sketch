d27077d574f8c7d81a0bbb96b5eb3c6874d04be4
Noah Kingrey <ivorykingrey@gmail.com>
2023-06-04 22:41:38 -0500

    edit index size input and script createBoard

--- script.js
+++ script.js
@@ -1,21 +1,23 @@
 
-const boardSpace = document.querySelector('#board')
-const size = document.querySelector('#size')
+const boardSpace = document.querySelector('#board');
+const size = document.querySelector('#size');
+let basis = createBoard()
 
 size.addEventListener('mousemove', function(){
     while (boardSpace.firstChild) {
         boardSpace.removeChild(boardSpace.firstChild);
-      }
-    createBoard(size.value)
-})
+    }
+    createBoard(size.value);
+});
 
 function createBoard(size){
-    for(i = 1; i <= size; i++){
+    let area = (size ** 2)
+    for(i = 1; i <= area; i++){
         let pixel = document.createElement('div');
         pixel.textContent = 'Div' + i;
         pixel.classList.add('pixel')
         boardSpace.appendChild(pixel);
-    }
+    };
 }
 
 createBoard(16)
